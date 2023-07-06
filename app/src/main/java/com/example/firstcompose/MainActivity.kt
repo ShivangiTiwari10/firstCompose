37fd5e1bd79987398f51511d99217671ad5d11a1
Shivangi <shivangit1111@gmail.com>
2023-07-06 19:16:31 +0530

    feat: show circle image

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
@@ -13,6 +15,10 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
@@ -23,6 +29,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
@@ -32,7 +39,11 @@ class MainActivity : ComponentActivity() {
         setContent {
 //            Text(text = "HELLO COMPOSE")
 //            SayCheesy2("Jaspreet")
-            TextInput()
+//            TextInput()
+//            LayoutComposable()
+//            PreviewFunction()
+
+            CircularImage()
         }
     }
 }
@@ -80,8 +91,31 @@ fun SayCheesy2(name: String) {
 
 @Preview(showBackground = true, name = "Hello msg2", widthDp = 250, heightDp = 400)
 @Composable
-fun PreviewFunction() {
-    SayCheesy2("Keshmeera")
+private fun PreviewFunction() {
+//    SayCheesy2("Keshmeera")
+    Column() {
+        ListViewItems(R.drawable.arrow, "Ranjeeta", "Bike rider")
+        ListViewItems(R.drawable.arrow, "Savita", "Singer")
+        ListViewItems(R.drawable.arrow, "Babita", "technician")
+        ListViewItems(R.drawable.arrow, "Rohini", "Tester")
+        ListViewItems(R.drawable.arrow, "Praneeta", "professor")
+        ListViewItems(R.drawable.arrow, "Anjali", "Teacher")
+    }
+
+//    CUsTOM Composable using modifier
+//    Text(
+//        text = "Hello", color = Color.White,
+//        modifier = Modifier
+//            .clickable { }
+//            .background(Color.Red)
+//            .size(100.dp)
+//            .border(4.dp, Color.Yellow)
+//            .clip(CircleShape)
+//            .padding(12.dp, 11.dp)
+//            .background(Color.Black)
+//
+//    )
+
 }
 
 @Preview(showBackground = true, name = "Hello msg2", widthDp = 250, heightDp = 400)
@@ -104,11 +138,87 @@ fun TextInput() {
         mutableStateOf("")
     }
     TextField(
-        value = state.value ,
+        value = state.value,
         onValueChange =
-        {state.value = it
+        {
+            state.value = it
         },
         label = { Text(text = "Enter Message") },
     )
 }
 
+// 5.Layout Composable
+
+@Preview(showBackground = true, widthDp = 400, heightDp = 500)
+@Composable
+private fun LayoutComposable() {
+
+//    a.COLUMN Column mimic Linear layout (arrange vertically)
+// Column(
+//     verticalArrangement = Arrangement.SpaceEvenly,
+//     horizontalAlignment = Alignment.CenterHorizontally
+// ) {
+//         Text(text = "ColumnA", fontSize = 20.sp, fontFamily = FontFamily.Serif)
+//         Text(text = "ColumnB", fontSize = 15.sp)
+// }
+//   b.ROW (arrange horizontally)
+//    Row(
+//        horizontalArrangement =Arrangement.SpaceAround,
+//         verticalAlignment = Alignment.CenterVertically
+//
+//    ) {
+//        Text(text = "Row1", fontSize = 20.sp, fontFamily = FontFamily.Serif)
+//        Text(text = "Row2", fontSize = 15.sp)
+//    }
+
+
+//    c.Box (just like frame layout)
+//      Box() {
+//
+//          Image(painter = painterResource(id = R.drawable.heart), contentDescription ="" )
+//          Image(painter = painterResource(id = R.drawable.ic_baseline_arrow_outward_24), contentDescription ="" )
+//      }
+//
+}
+
+// 6. ListView Item
+@Composable
+fun ListViewItems(ImagId: Int, name: String, ocupation: String) {
+
+
+    Row(Modifier.padding(8.dp)) {
+        Image(painter = painterResource(id = ImagId), contentDescription = "", Modifier.size(45.dp))
+        Column(
+            Modifier
+                .padding(8.dp)
+        ) {
+
+            Text(
+                text = name, fontSize = 20.sp, fontWeight
+                = FontWeight.SemiBold
+            )
+            Text(
+                text = ocupation,
+                fontSize = 20.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Thin
+            )
+        }
+    }
+}
+
+@Composable
+fun CircularImage() {
+    Image(
+        painter = painterResource(id = R.drawable.bird),
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(100.dp)
+            .clip(CircleShape)
+            .border(2.dp, Color.LightGray)
+            .padding(5.dp)
+            .shadow(8.dp),
+        contentDescription = ""
+    )
+
+}
